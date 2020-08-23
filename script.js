5149338c868ee375af3578771cb893998531792e
Deepak Chauhan <rishudc119@gmail.com>
2020-08-23 17:35:45 +0000

    added style

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ function confirmPassword(){
   let password1=document.getElementById("passwordOne").value
   let password2=document.getElementById("passwordTwo").value
   let matchStatus1=document.getElementById("matchStatus")
-  let matchStatus= "not matched";
+  var matchStatus= "Not matched";
 
   if((password1 == password2)&& (password1.length>6)&&(password2.length>6)){
     matchStatus= "password matched";
 
   }else {
-    matchStatus="not matched";
+    matchStatus="Not matched";
   }
-matchStatus1.className=matchStatus.toLowerCase();
+matchStatus1.className=matchStatus.toLowerCase().replace(' ','-');
 matchStatus1.innerText=matchStatus;
-
-
 }
